be72c726339286e40aded30988276b048dc3cb79
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-01-18 19:58:04 +0600

    02-Variables and use strict

--- js-basics/app.js
+++ js-basics/app.js
@@ -1,11 +1,26 @@
-// Instructions
-console.log("Hello!");
-console.log("Hello,", "World!");
+"use strict";
 
-// This is a comment
+/**
+ * Variables and use strict
+ */
 
-/*
-  This is a
-  multiline
-  comment
-*/
+// Declare variable with value
+let message = "Hello!";
+console.log(message);
+
+// Change value of the variable
+message = "Bye!";
+console.log(message);
+
+// Old way of declaring variable
+var message2 = "Hi!";
+console.log(message2);
+
+// Constant variables
+const message3 = "Good Day!";
+console.log(message3);
+
+// Don't change value of const variable
+// message3 = 'Some new value';
+
+// Don't use reserved words as variable names
