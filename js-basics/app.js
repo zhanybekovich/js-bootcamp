0733c60ee65a8da0120c16be949fc32bc0403ed2
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-01-23 19:46:04 +0600

    13-Number methods

--- js-basics/app.js
+++ js-basics/app.js
@@ -1,36 +1,71 @@
 "use strict";
 
 /**
- * this
+ * Primitive methods
  */
 
-console.log("Global this: ", this);
+// Round
+const price = 99.99;
+const roundedPrice = price.toFixed(1);
 
-function foo() {
-  console.log("this in foo: ", this);
-}
+console.log(roundedPrice);
 
-foo();
+// To precision
+console.log((100.055).toPrecision(4));
 
-console.log("==========");
+// To string
+console.log(price.toString());
 
-const user = {
-  name: "Bob",
-  age: 20,
-  logThis: function () {
-    console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old`);
-  },
+// Math
+console.log(Math);
 
-  address: {
-    city: "New York",
-    zipCode: "12345",
-    logInnerThis: function () {
-      console.log(`I live in ${this.city} with the zip code ${this.zipCode}`);
-    },
-  },
-};
+console.log("======");
 
-user.logThis();
-user.address.logInnerThis();
+// Random
+console.log(Math.floor(Math.random() * 10) + 1);
 
-console.log("==========");
+// Abs
+console.log(Math.abs(-10));
+
+// Power
+console.log(Math.pow(2, 3));
+
+// Sqrt
+console.log(Math.sqrt(9));
+
+// Min
+console.log(Math.min(1, 2, 3, 4, 5));
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(Math.min(...numbers));
+
+// Max
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.max(...numbers));
+
+// Round
+console.log(Math.round(1.4));
+console.log(Math.round(1.5));
+console.log(Math.round(1.6));
+
+// Floor
+console.log(Math.floor(1.4));
+
+// Ceil
+console.log(Math.ceil(1.4));
+
+// Trunc
+console.log(Math.trunc(1.4));
+
+// Convert to number
+console.log(Number("123"));
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number(null));
+console.log(Number(""));
+
+// ParseInt
+console.log(parseInt("100px"));
+
+// ParseFloat
+console.log(parseFloat("100px"));
