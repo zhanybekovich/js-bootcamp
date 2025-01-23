838dfa33d139b86a3692224715ac5f41390f2786
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-01-23 20:17:44 +0600

    14-String methods

--- js-basics/app.js
+++ js-basics/app.js
@@ -1,71 +1,87 @@
 "use strict";
 
 /**
- * Primitive methods
+ * String methods
  */
 
-// Round
-const price = 99.99;
-const roundedPrice = price.toFixed(1);
+const str = "Hello world!";
 
-console.log(roundedPrice);
+// length
+console.log(str.length);
 
-// To precision
-console.log((100.055).toPrecision(4));
+// symbol by index
+console.log(str[0]);
+console.log(str[4]);
 
-// To string
-console.log(price.toString());
+// last symbol
+console.log(str[str.length - 1]);
 
-// Math
-console.log(Math);
+// at
+console.log(str.at(0));
+console.log(str.at(-1));
 
-console.log("======");
+// toLowerCase
+console.log(str.toLowerCase());
 
-// Random
-console.log(Math.floor(Math.random() * 10) + 1);
+// toUpperCase
+console.log(str.toUpperCase());
 
-// Abs
-console.log(Math.abs(-10));
+// trim start
+console.log(str.trimStart());
 
-// Power
-console.log(Math.pow(2, 3));
+// trim end
+console.log(str.trimEnd());
 
-// Sqrt
-console.log(Math.sqrt(9));
+// trim
+console.log(str.trim());
 
-// Min
-console.log(Math.min(1, 2, 3, 4, 5));
+// index of
+console.log(str.indexOf("l"));
+console.log(str.indexOf("l", 5));
 
-const numbers = [1, 2, 3, 4, 5];
-console.log(Math.min(...numbers));
+// includes
+console.log(str.includes("l"));
+console.log(str.includes("l", 5));
 
-// Max
-console.log(Math.max(1, 2, 3, 4, 5));
-console.log(Math.max(...numbers));
+// startsWith
+console.log(str.startsWith("Hello"));
 
-// Round
-console.log(Math.round(1.4));
-console.log(Math.round(1.5));
-console.log(Math.round(1.6));
+// endsWith
+console.log(str.endsWith("!"));
 
-// Floor
-console.log(Math.floor(1.4));
+// repeat
+console.log(str.repeat(3));
 
-// Ceil
-console.log(Math.ceil(1.4));
+// substring
+console.log(str.substring(0, 5));
+console.log(str.substring(3, 0));
 
-// Trunc
-console.log(Math.trunc(1.4));
+// slice
+console.log(str.slice(0, 5));
+console.log(str.slice(-3));
+console.log(str.slice(-3, -1));
 
-// Convert to number
-console.log(Number("123"));
-console.log(Number(true));
-console.log(Number(false));
-console.log(Number(null));
-console.log(Number(""));
+// replace
+console.log(str.replace("world", "JavaScript"));
+console.log(str.replaceAll("world", "JavaScript"));
+console.log(str.replace(/world/g, "JavaScript"));
+console.log(str.replace(/world/gi, "JavaScript"));
 
-// ParseInt
-console.log(parseInt("100px"));
+const str2 = "+996 (555) 555-55-55";
+console.log(str2.replace(/\d/g, "*"));
 
-// ParseFloat
-console.log(parseFloat("100px"));
+// split
+console.log(str2.split(" "));
+console.log(str2.split(""));
+
+// challenge
+const value = " john smith ";
+const clearValue = value.trim().toLowerCase();
+
+if (clearValue.length === 0) {
+  console.log("Error! Enter your name");
+}
+
+if (clearValue.includes("admin")) {
+  console.log("This name is already taken!");
+}
