5bbddcab3ebf4a2e77e27eb6ae3cc604be0aad4c
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-01-23 17:14:27 +0600

    12-This

--- js-basics/app.js
+++ js-basics/app.js
@@ -1,116 +1,36 @@
 "use strict";
 
 /**
- * Objects: copying, joining, destruction
+ * this
  */
 
-// Objects are not equivalent
-const obj1 = { age: 10 };
-const obj2 = { age: 0 };
+console.log("Global this: ", this);
 
-console.log(obj1 === obj2);
+function foo() {
+  console.log("this in foo: ", this);
+}
 
-// Reference
-const obj3 = { name: "Alex" };
-const obj4 = obj3;
+foo();
 
-console.log(obj3 === obj4);
+console.log("==========");
 
-// Checking if objects are equal
-const obj5 = {
-  name: "Alex",
+const user = {
+  name: "Bob",
   age: 20,
-};
+  logThis: function () {
+    console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old`);
+  },
 
-const obj6 = {
-  name: "Alex",
-  age: 20,
-};
-
-const areObjectsEqual = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-
-  for (const key in obj1) {
-    if (obj1[key] !== obj2[key]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-console.log(areObjectsEqual(obj5, obj6));
-
-// Cloning objects
-
-const obj7 = { name: "Alex", age: 20 };
-
-// const obj8 = Object.assign({}, obj7);
-
-// or
-const obj8 = { ...obj7 };
-
-// Joining objects
-const obj9 = { name: "Alex", age: 20 };
-const obj10 = { job: "Developer" };
-
-const user = Object.assign({}, obj9, obj10);
-console.log(user);
-
-// Optional chaining
-
-const user2 = {
-  name: "John",
-  age: 30,
   address: {
-    // city: "New York",
-    zipCode: 10001,
+    city: "New York",
+    zipCode: "12345",
+    logInnerThis: function () {
+      console.log(`I live in ${this.city} with the zip code ${this.zipCode}`);
+    },
   },
 };
 
-console.log(user2.address?.city);
+user.logThis();
+user.address.logInnerThis();
 
-// Destructuring
-
-const user3 = {
-  name: "John",
-  age: 30,
-  city: "New York",
-};
-
-const { name, age } = user3;
-console.log(name, age);
-
-// Renaming at destruction
-const { name: username } = user3;
-console.log(username);
-
-// Default values
-const { country = "Unknown" } = user3;
-
-console.log(country);
-
-// Rest operator
-const logUser = (user) => {
-  const { name, age, city, ...otherInfo } = user;
-
-  console.log(`
-    Name: ${name}
-    Age: ${age}
-    City: ${city}`);
-
-  console.log(otherInfo);
-};
-
-logUser({
-  name: "John",
-  age: 30,
-  city: "New York",
-  company: "Google",
-  country: "USA",
-});
+console.log("==========");
