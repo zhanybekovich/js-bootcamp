44aad817b35ef253abe75edbed3aae5927c1adc0
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-01-28 18:58:15 +0600

    17-JSON

--- js-basics/app.js
+++ js-basics/app.js
@@ -1,53 +1,27 @@
 "use strict";
 
 const user = {
-  name: "John",
+  name: "Alex",
   age: 25,
   city: "New York",
+  address: {
+    street: "45, Broadway",
+    zipcode: 123456,
+  },
+  todos: ["sleep", "eat", "work", "train"],
+  hasCat: true,
+  yacht: null,
 };
 
-const userKeys = Object.keys(user);
-console.log(userKeys);
+// const userDataAsString = user.toString();
+// console.log(userDataAsString);
 
-const userValues = Object.values(user);
-console.log(userValues);
+const userDataAsString = JSON.stringify(user);
+console.log(userDataAsString);
 
-const userEntries = Object.entries(user);
-console.log(userEntries);
+console.log("======");
 
-userEntries.forEach(([key, value]) => {
-  console.log("Key:", key);
-  console.log("Value:", value);
-});
+const parsedUserData = JSON.parse(userDataAsString);
+console.log(parsedUserData);
 
-const formattedUserEntries = userEntries.map(([key, value]) => {
-  return [key.toUpperCase(), `--${value}--`];
-});
-
-console.log(formattedUserEntries);
-
-const userFormatted = Object.fromEntries(formattedUserEntries);
-console.log(userFormatted);
-
-// Map
-const data = new Map([
-  [1, "Number 1"],
-  ["1", "as string 1"],
-]);
-
-console.log(data);
-
-// dynamically set values
-const data2 = new Map();
-
-data2.set(1, "1 as number");
-data2.set("1", "as a string");
-
-console.log(data2);
-
-// get
-console.log(data.get(1));
-console.log(data.get("1"));
-
-// check
-console.log(data.has("1"));
+console.log("======");
